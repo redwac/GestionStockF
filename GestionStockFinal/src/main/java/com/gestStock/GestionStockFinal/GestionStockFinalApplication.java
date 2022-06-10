18bd7c6e111c998a3cab39d7e12eac71c9effec9
redwac <sabbane.reda@gmail.com>
2022-06-10 17:58:51 +0100

    'masetr'

---
+++ GestionStockFinal/src/main/java/com/gestStock/GestionStockFinal/GestionStockFinalApplication.java
@@ -0,0 +1,13 @@
+package com.gestStock.GestionStockFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockFinalApplication.class, args);
+	}
+
+}
